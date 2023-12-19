8e2f5bfe34db11fae376398f94cd8ab30459fb5f
deeptiverma2012 <2012deeptiv@gmail.com>
2023-12-19 13:34:39 -0600

    feature 2 added

---
+++ src/Feature2/loops2.java
@@ -0,0 +1,14 @@
+package Feature2;
+
+public class loops2 {
+
+	public static void main(String[] args) {
+	 int i=1;
+	 while(i<10) {
+		 System.out.println("I am a batman"+" "+i);
+		 i++;
+	 }
+
+	}
+
+}
